cc16b4f1501a1ce058e9457fc76c8735437d0cdb
SONALI619 <sonalibera366@gmail.com>
2025-06-07 19:54:32 +0530

    Question9

---
+++ src/Assignment_june_7/question9.kt
@@ -0,0 +1,9 @@
+package Assignment_june_7
+
+fun main(){
+    val age = 65
+    if (age >= 60)
+        println("You’re eligible for a senior discount!")
+    else
+        println("Sorry, no discount yet!")
+}
