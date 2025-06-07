1070186661030ce10cac64380eec8416659144d6
SONALI619 <sonalibera366@gmail.com>
2025-06-07 20:41:52 +0530

    Question16

---
+++ src/Assignment_june_7/question16.kt
@@ -0,0 +1,9 @@
+package Assignment_june_7
+
+fun main(){
+    val orderName = "Latte"
+    if (orderName.length >= 5)
+        println("Nice long name for your coffee!")
+    else
+        println("Short and sweet coffee name!")
+}
