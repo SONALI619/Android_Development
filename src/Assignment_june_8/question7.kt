1fef6236a60a2dd274580b23ee269d5fa2e2138c
SONALI619 <sonalibera366@gmail.com>
2025-06-08 20:20:28 +0530

    q7

---
+++ src/Assignment_june_8/question7.kt
@@ -0,0 +1,10 @@
+package Assignment_june_8
+
+fun sumMessage(x: Int, y: Int): String {
+    val sum = x + y
+    return "Sum of $x and $y is $sum"
+}
+
+fun main() {
+    println(sumMessage(38, 58))
+}
