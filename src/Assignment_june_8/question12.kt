09ac39edd1bb90d925bd27eb61d2084685f89130
SONALI619 <sonalibera366@gmail.com>
2025-06-09 11:17:28 +0530

    q12

---
+++ src/Assignment_june_8/question12.kt
@@ -0,0 +1,10 @@
+package Assignment_june_8
+
+fun main() {
+    val number: Int? = 10
+    if (number != null) {
+        println("The value is $number")
+    } else {
+        println("Value is missing")
+    }
+}
