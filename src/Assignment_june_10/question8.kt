f2277eb5b4e2187fa9319b33a2a425c77db1ad6f
SONALI619 <sonalibera366@gmail.com>
2025-06-11 18:53:27 +0530

    q8

---
+++ src/Assignment_june_10/question8.kt
@@ -0,0 +1,9 @@
+package Assignment_june_10
+
+class Person(val name: String, val age: Int)
+
+fun main() {
+    val person = Person("Sonali", 19)
+    println("Name: ${person.name}")
+    println("Age: ${person.age}")
+}
