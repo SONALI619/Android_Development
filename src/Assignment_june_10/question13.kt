1e68fc675ad0af93861efe956c6a25fa0c786a88
SONALI619 <sonalibera366@gmail.com>
2025-06-11 19:05:10 +0530

    q13

--- src/Assignment_june_10/question13.kt
+++ src/Assignment_june_10/question13.kt
@@ -1,2 +1,9 @@
 package Assignment_june_10
 
+data class User(val name: String, val email: String)
+
+fun main() {
+    val u1 = User("Sonali", "s@gmail.com")
+    val u2 = User("Nisha", "n@gmail.com")
+    println(u1 == u2)
+}
