7d7d3a1c1f9a97f8deeb472b221ef1c52d462a31
SONALI619 <sonalibera366@gmail.com>
2025-06-11 19:07:07 +0530

    q14

---
+++ src/Assignment_june_10/question14.kt
@@ -0,0 +1,11 @@
+package Assignment_june_10
+
+object Logger {
+    fun log(message: String) {
+        println("Log: $message")
+    }
+}
+
+fun main() {
+    Logger.log("Hotspot Connected Successfully")
+}
