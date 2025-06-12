bd98a3d9b33ffcbc2d0e05cff3bf56a7ae61fc95
SONALI619 <sonalibera366@gmail.com>
2025-06-12 19:47:32 +0530

    q15

---
+++ src/Assignment_june_10/question15.kt
@@ -0,0 +1,13 @@
+package Assignment_june_10
+
+class MathUtils {
+    companion object {
+        fun square(n: Int): Int {
+            return n * n
+        }
+    }
+}
+
+fun main() {
+    println(MathUtils.square(6))
+}
